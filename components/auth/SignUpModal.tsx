10d3483dd1053c94e7069c324f06dfe49f69e704
kwhong95 <kwhong95@kakao.com>
2022-04-25 20:23:00 +0900

    공통 인풋 컴포넌트 생성 및 회원가입 모달 컴포넌트에 적용하기

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -1,11 +1,13 @@
 import styled from "styled-components";
-import palette from "../../styles/palette";
 import {
   AiOutlineClose,
   AiOutlineMail,
   AiOutlineEyeInvisible,
 } from "react-icons/ai";
 import { BsPerson } from "react-icons/bs";
+
+import { Input } from "../common";
+import palette from "../../styles/palette";
 
 const Container = styled.div`
   width: 568px;
@@ -49,20 +51,25 @@ export const SignUpModal: React.FC = () => {
     <Container>
       <AiOutlineClose className="modal-close-x-icon" />
       <div className="input-wrapper">
-        <input placeholder="이메일 주소" type="email" name="email" />
-        <AiOutlineMail />
+        <Input
+          placeholder="이메일 주소"
+          type="email"
+          name="email"
+          icon={<AiOutlineMail />}
+        />
       </div>
       <div className="input-wrapper">
-        <input placeholder="이름(예:길동)" />
-        <BsPerson />
+        <Input placeholder="이름(예:길동)" icon={<BsPerson />} />
       </div>
       <div className="input-wrapper">
-        <input placeholder="성(예: 홍)" />
-        <BsPerson />
+        <Input placeholder="성(예: 홍)" icon={<BsPerson />} />
       </div>
       <div className="input-wrapper">
-        <input placeholder="비밀번호 설정하기" type="password" />
-        <AiOutlineEyeInvisible />
+        <Input
+          placeholder="비밀번호 설정하기"
+          type="password"
+          icon={<AiOutlineEyeInvisible />}
+        />
       </div>
     </Container>
   );
